17232b8ddfdf0e90f0b5c714666a15fb95c21c91
Bambam <bam.ksnru@gmail.com>
2025-08-08 17:20:56 +0700

    fix: question 3

--- question-3.js
+++ question-3.js
@@ -1,8 +1,12 @@
 // เริ่มเขียนโค้ดตรงนี้
 async function getUser(){
+    try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const users = await response.json();
-        const longName = users.map(users => users.name).filter(name => name.length > 17);
+        const longName = users.map(user => user.name).filter(name => name.length > 17);
         console.log(longName);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+    }
 }
 getUser();
